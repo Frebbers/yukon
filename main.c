26bc919494699a09d3be7a03485e1cb0d1d656a4
Frebbers <frederik.bode@hendrichsen.dk>
2024-03-24 13:57:59 +0100

    Finished card struct

--- main.c
+++ main.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
+
+
 typedef struct {
     int* suit;
     int* value;
     struct Card* nextCard;
-    
+    int isFaceUp;
 
 } Card;
 
 int main() {
 
-    int n = 4;
-    int *lars = &n;
 
-    //"*var =" kaldes referencing; fortæller C at var er en pointer
-    //" = *var" kaldes dereferencing; hvilken værdi peger var på?
-    //&var addressen af en variable
-    int t = *lars
-
-
-
-
-
-
-    printf("n = %d, lars = %d", n, *lars);
 
     return 0;
 
