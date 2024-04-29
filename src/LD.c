c12c925c1f9152ecf188f312363f5a9228c28436
Frebbers <frederik.bode@hendrichsen.dk>
2024-04-29 20:59:01 +0200

    Deckloader now loads the face texture to the card

--- src/LD.c
+++ src/LD.c
@@ -4,8 +4,9 @@
 #include "carddeck.h"
 #include "stdlib.h"
 #include "string.h"
+#include "renderTools.h"
 
-Card* loadDeck(const char *filename) {
+Card* loadDeck(const char *filename, SDL_Renderer *renderer) {
     FILE *file = fopen(filename, "r");
     char *message;
     if (file == NULL) {
@@ -17,6 +18,8 @@ Card* loadDeck(const char *filename) {
 
     while (fscanf(file, "%2s", buffer) == 1) {
         Card *newCard = createCard(buffer[0], buffer[1]);
+        newCard -> isFaceUp = 1;
+        applyCardTexture(renderer, newCard);
         if (head == NULL) {
             head = newCard;
             tail = newCard;
--- src/LD.h
+++ src/LD.h
@@ -5,7 +5,7 @@
 #ifndef UNTITLED_LD_H
 #define UNTITLED_LD_H
 
-Card* loadDeck(const char *filename);
+Card* loadDeck(const char *filename, SDL_Renderer *renderer);
 
 
 #endif //UNTITLED_LD_H
